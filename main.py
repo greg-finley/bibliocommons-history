74c81bc9d65c5a5af7de9b1cfa72079ab6730e70
Greg Finley <gregory.finley@gmail.com>
2023-07-18 21:24:58 -0700

    Parse more

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import json
 with open("/Users/gregoryfinley/Desktop/response.json") as json_file:
     data = json.load(json_file)
 
+entities = data["entities"]
+bibs = entities["bibs"]
+borrowing_history = entities["borrowingHistory"]
+checked_outs = {}
+for b in borrowing_history.values():
+    checked_outs[b["metadataId"]] = b["checkedoutDate"]
+pagination = data["borrowing"]["borrowingHistory"]["pagination"]
 
-bibs = data["entities"]["bibs"]
-for i, bib in enumerate(bibs.values()):
+for k, bib in bibs.items():
     print(
         {
             "title": bib["briefInfo"]["title"],
-            "authors": bib["briefInfo"]["authors"],
+            "authors": "; ".join(bib["briefInfo"]["authors"]),
             "format": bib["briefInfo"]["format"],
+            "checkedOut": checked_outs[k],
         }
     )
+
+print(pagination)
